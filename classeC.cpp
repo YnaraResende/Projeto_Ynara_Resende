48164dca0397c1f381e679a6f098abdcd3ffdd29
Ynara Resende <ynara.faria@estudante.ufla.br>
2019-09-09 16:35:15 -0300

    Revert "Revert "Create classeC.cpp""
    This reverts commit 10a52f4ebe59e17f634d3d8b7a64ccaf8663cf30.

---
+++ classeC.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class C{
+	private:
+		string C1;
+		int C2;
+
+	public:
+		
+		void MC1(){
+			cout<<"MC1"<<endl;
+		}
+	
+		void MC2(){
+			cout <<"MC2"<<endl;
+		}
+		
+};
