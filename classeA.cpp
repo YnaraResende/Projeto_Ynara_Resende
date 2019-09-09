5cb2f7ba778719763440073a7ebb776591cfed5d
Ynara <55094288+YnaraResende@users.noreply.github.com>
2019-09-09 15:15:46 -0300

    Create classeA.cpp

---
+++ classeA.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class A{
+	private:
+		int A1;
+		float A2;
+
+	public:
+		int getA1(){
+			return A1;
+		}
+	
+		float getA2(){
+			return A2;
+		}
+	
+		void setA1(int _A1){
+			A1 = _A1;
+		}
+	
+		void setA2(float _A2){
+			A2 = _A2;
+		}
+	
+		void MA1(){
+			cout<<"MA1"<<endl;
+		}
+	
+		void MA2(){
+			cout <<"MA2"<<endl;
+		}
+		
+};
