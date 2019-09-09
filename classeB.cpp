4b13997aeee05eca88dec29e0914e346f4cf5324
Ynara <55094288+YnaraResende@users.noreply.github.com>
2019-09-09 15:26:13 -0300

    Create classeB.cpp

---
+++ classeB.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class B{
+	private: int B1;
+			 float B2;
+
+	public: 
+		int getB1(){
+			return B1;
+		}
+	
+		float getB2(){
+			return B2;
+		}
+	
+		void setB1(int _B1){
+			B1 = _B1;
+		}
+	
+		void setB2(float _B2){
+			B2 = _B2;
+		}
+	
+		void MB1(){
+			cout<<"MB1"<<endl;
+		}
+	
+		void MB2(){
+			cout <<"MB2"<<endl;
+		}
+		
+};
